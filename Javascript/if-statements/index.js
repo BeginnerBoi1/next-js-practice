afb1ef87b7571f227a47c7a6a39b17c4f60d0e6f
BeginnerBoi1 <santoschinkit@gmail.com>
2025-12-10 03:58:20 +0800

    feat: add HTML and JavaScript files for if-statements practice with basic conditional logic

---
+++ Javascript/if-statements/index.js
@@ -0,0 +1,16 @@
+// if statements
+/*
+
+if (condition) {
+    action
+}
+
+*/
+let age = 0;
+if (age <= 0){
+    console.log("You don't exist yet!");
+}else if (age < 18){
+    console.log("You're too young!");
+}else{
+    console.log("You're in!");
+}
