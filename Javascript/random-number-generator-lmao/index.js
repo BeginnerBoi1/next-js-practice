ee172a61392259d8f1d48dc7f52098e7b86fe7de
BeginnerBoi1 <santoschinkit@gmail.com>
2025-12-10 03:44:43 +0800

    feat: enhance random number generator UI and functionality with updated styles and instant number display

--- Javascript/random-number-generator-lmao/index.js
+++ Javascript/random-number-generator-lmao/index.js
@@ -1,2 +1,8 @@
 // Random number generator
-let randomNumber = Math.random // This generates a random number
+let randomNumber = Math.floor(Math.random() * 6); // This generates a random number
+const randomButton = document.getElementById("random-button");
+
+randomButton.onclick = function(){
+    document.getElementById("randomNumber").textContent = randomNumber;
+    randomNumber = Math.floor(Math.random() * 11);
+}
