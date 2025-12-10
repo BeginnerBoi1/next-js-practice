42d395babf0c5adb1f3274549ab6d5859c3f7859
BeginnerBoi1 <santoschinkit@gmail.com>
2025-12-10 13:14:36 +0800

    feat: implement subscription and payment selection logic in if-statements practice

--- Javascript/if-statements/index.js
+++ Javascript/if-statements/index.js
@@ -14,3 +14,34 @@ if (age <= 0){
 }else{
     console.log("You're in!");
 }
+
+// Subscribe script
+
+// Declare the necessary variables
+const subresult = document.getElementById("result-text-subscribe")
+const paymentresult = document.getElementById("result-text-payment")
+const subscribeBtn = document.getElementById("subscribeBtn")
+const visaBtn = document.getElementById("visaBtn")
+const paypalBtn = document.getElementById("paypalBtn")
+const paymeBtn = document.getElementById("paymeBtn")
+const submitBtn = document.getElementById("submitBtn")
+
+submitBtn.onclick = function() {
+
+    if (subscribeBtn.checked){
+        subresult.textContent = `You are subscribed!`
+    }else{
+        subresult.textContent = `You are not subscribed!`
+    }
+
+    // Payment
+    if (visaBtn.checked){
+        paymentresult.textContent = "You are paying with Visa"
+    }else if (paypalBtn.checked){
+        paymentresult.textContent = "You are paying with paypal"
+    }else if (paymeBtn.checked){
+        paymentresult.textContent = "You are paying with payMe"
+    }else{
+        paymentresult.textContent = "You must select a payment type!"
+    }
+}
